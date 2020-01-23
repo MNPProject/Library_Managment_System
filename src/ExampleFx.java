f82518509936397d800390b72c5db0f9a237d8e9
gutama8787 <gutama8787@gmail.com>
2020-01-22 23:01:43 -0500

    admin log in fixed to verfiy existence of user input name and password

--- src/ExampleFx.java
+++ src/ExampleFx.java
@@ -1,21 +1,23 @@
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class ExampleFx extends Application{
 
 	// may not be necessary 
-	Scene EntryView,
-	adminSection,LibrarianSection,
-	addBook,addLib,
-	adminLogin, libLogin,
-	returnBook, deleteBook;
+	
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -23,13 +25,19 @@ public class ExampleFx extends Application{
 		Parent root = FXMLLoader.load(getClass().getResource("EntryView.fxml"));
 		
 		
-		EntryView = new Scene(root, 600, 600);
+		Scene  EntryView = new Scene(root, 600, 600);
 		primaryStage.setScene(EntryView);
 		primaryStage.show();
 	}
+	/**
+	 * This scene appears if a user chooses the admin log in button.
+	 * 
+	 * @param event
+	 * @throws IOException
+	 */
 	public void adminLogin(ActionEvent event) throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource("AdminLoginForm.fxml"));
-		adminLogin = new Scene(root);
+		Scene adminLogin = new Scene(root);
 		
 		
 		Stage stage= getStage(event);
@@ -37,28 +45,73 @@ public class ExampleFx extends Application{
 		stage.setScene(adminLogin);
 		stage.show();
 	}
+	
 	public void libLogin(ActionEvent event) throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource("LibrarianLoginForm.fxml"));
 		Scene scene = new Scene(root);
 		
+		System.out.println(event.getTarget().toString());
 		
 		Stage stage= getStage(event);
 		
 		stage.setScene(scene);
 		stage.show();
 	}
-	public void adminSection(ActionEvent event) throws IOException {
+	@FXML PasswordField adminPassword;
+	@FXML TextField adminName;
+	boolean adminFound = false;;
+	@FXML public void adminLoginCheck(ActionEvent event) {
+		// TODO 
+		
+		// check for admin's name existence 
+		// how? pull admin's name from a database, a set, list
+		// then
+		// check for password match
+		
+		// for now a weak test; assuming admins are stored in a list
+		// checks every admin in list, compares name and password with user input
+		Admin admin1 = new Admin("gutama","password");
+		ArrayList<Admin> adminList = new ArrayList<Admin>();
+		adminList.add(admin1);
+		
+		
+		
+		adminList.forEach(admin->{
+			// if true proceeds to the next page
+			if(admin.getName().equals(adminName.getText()) && admin.getPassword().equals(adminPassword.getText())){
+				
+				try {
+					adminFound = true;
+					adminSection(event);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				return;
+			}
+		});
+		
+		// else shows error
+		if(!adminFound) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Error Dialog");
+			alert.setHeaderText("Look, an Error Dialog");
+			alert.setContentText("password is:"+adminList.get(0).getName()+" "+adminList.get(0).getPassword());
+
+			alert.showAndWait();
+		}
+		
+		
+	}
+	 public void adminSection(ActionEvent event) throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource("AdminSection.fxml"));
-		adminSection = new Scene(root);
+		Scene adminSection = new Scene(root);
+		
 		Stage stage = getStage(event);
 		stage.setScene(adminSection);
 		stage.show();
 	}
-	private Stage getStage(ActionEvent event) {
-		Node node = (Node) event.getSource();
-		Stage stage= (Stage) node.getScene().getWindow();
-		return stage;
-	}
+	
 	public void LibrarianSection(ActionEvent event) throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource("LibrarianSection.fxml"));
 		Scene scene = new Scene(root);
@@ -67,6 +120,11 @@ public class ExampleFx extends Application{
 		stage.show();
 	}
 	
+	private Stage getStage(ActionEvent event) {
+		Node node = (Node) event.getSource();
+		Stage stage= (Stage) node.getScene().getWindow();
+		return stage;
+	}
 	public static void main(String[] args) {
 		launch();
 	}
