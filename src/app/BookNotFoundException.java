05da99ff1f70a74dfded50fd68c2b84c2a73c6f5
gutama8787 <gutama8787@gmail.com>
2020-05-25 16:31:13 -0400

    con

---
+++ src/app/BookNotFoundException.java
@@ -0,0 +1,14 @@
+package app;
+
+
+
+public class BookNotFoundException extends Exception {
+
+	public BookNotFoundException() {
+		System.out.println("Error, The book you are trying to issue does not exist");
+	}
+	
+	public BookNotFoundException(Book book) {
+		System.out.println("Error, \"" + book.getTitle() + "\" book has 0 quantity. Try again later");
+	}
+}
