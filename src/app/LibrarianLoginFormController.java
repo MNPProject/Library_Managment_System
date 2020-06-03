3b6dccd3ebccd83f8f1e0685a14c4a085ed52ff2
AhaduTeddy <ahadyilma@gmail.com>
2020-06-03 16:29:38 -0400

    librarian login complete

--- src/app/LibrarianLoginFormController.java
+++ src/app/LibrarianLoginFormController.java
@@ -16,6 +16,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -43,65 +44,39 @@ public class LibrarianLoginFormController {
     void login(ActionEvent event) throws IOException {
     	
 
-    	class Employee {
-        	String username;
-        	String password;
-        	   	
-        	public Employee(String username, String password) {
-        		this.username = username;
-        		this.password = password;
-        	}
-        	public String getUsername() {
-        		return username;
-        	}
-        	public void setUsername(String username) {
-        		this.username = username;
-        	}
-        	public String getPassword() {
-        		return password;
-        	}
-        	public void setPassword(String password) {
-        		this.password = password;
-        }
-    	}
-    	List<Employee> employeeList = new ArrayList<>();
-    	employeeList.add(new Employee("Ahadu" , "Addis"));
-		employeeList.add(new Employee("Gutema", "London"));
-		employeeList.add(new Employee("Mike", "Washington"));
-		employeeList.add(new Employee("Nati", "Paris"));
-		System.out.println(name.getText());
+    	List<Librarian> employeeList = FileLoader.loadLibrarianFromFile();
 		String username = name.getText();
 		String password_ = password.getText();
 		
 		for(int i = 0; i < employeeList.size(); i ++) {
 			if(employeeList.get(i) != null) {
-				if(username == employeeList.get(i).getUsername() && password_ == employeeList.get(i).getPassword()) {
+				if(username.equals(employeeList.get(i).getFirstName()) && password_.equals(employeeList.get(i).getLastName())) {
 					Parent root = FXMLLoader.load(getClass().getResource("LibrarianSection.fxml"));
 					Scene scene = new Scene(root,600,600);
 					Stage stage = (Stage) pane.getScene().getWindow();
 					stage.setScene(scene);
-		    	}
+				}
 			}
 			else {
-					Parent root = FXMLLoader.load(getClass().getResource("LibrarianLoginAlert.fxml"));
-			    	Scene scene = new Scene(root,600,600);
-			    	Stage stage = (Stage) pane.getScene().getWindow();
-			    	stage.setScene(scene);
-					
+					System.out.println("Alert");
+					Alert alert = new Alert(Alert.AlertType.ERROR, "Please reenter");
+					alert.showAndWait();
 				}
 		}
+			}
 		
     	
     	
     	// we give you a list of librarins
 	    	// 
-	    	Parent root = FXMLLoader.load(getClass().getResource("LibrarianSection.fxml"));
+	    	/* Parent root = FXMLLoader.load(getClass().getResource("LibrarianSection.fxml"));
 	    	Scene scene = new Scene(root,600,600);
 	    	Stage stage = (Stage) pane.getScene().getWindow();
 	    	stage.setScene(scene);
-	    	
+	    	*/
+		
 	    
 
     }
-}
+
    
